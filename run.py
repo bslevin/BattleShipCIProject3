8ef1c9fbb5f23c54df65cb48ac875c443c5abf82
bslevin <blogs82@yahoo.com>
2022-07-22 14:50:12 +0000

    cleaned code up for pep8

--- run.py
+++ run.py
@@ -1,22 +1,24 @@
-from colorama import init, Fore, Back, Style
 from random import randint
 from re import sub
+from colorama import init, Fore, Back, Style
+
 
 # Lookup table used to convert from column number to column letter
 COL_LOOKUP = "ABCDEFGH"
 
-# Filter ANSI escape sequences out of any text sent to stdout or stderr on Windows and replace with equiv. WIN32 calls
-# Set autoreset to True so that styles are automatically reset after every printed message
+# Filter ANSI escape sequences out of any text sent to stdout or stderr.
+# Set autoreset to True so that styles are automatically reset
 init(autoreset=True)
+
 
 # Holds the state of a player and their game board
 class GameBoard:
 
     def __init__(self, name: str):
-        # Only store first 16 characters of input name to ensure the printed nameplate looks nice
+        # 16 characters of input name to ensure the printed nameplate
         self.name = name[:16]
 
-        # Players have both a board and public-facing board to facilitate information hiding when displaying the board
+        # Players have both a board and public-facing board
         self.board = []
         self.public_board = []
 
@@ -24,10 +26,12 @@ class GameBoard:
 
         self.clear()
 
-    # Called to reset the game board, placing empty spaces at all coordinates
+    # Called to reset the game board, placing empty spaces
     def clear(self):
-        self.board = [[BOARD_WAVE for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
-        self.public_board = [[BOARD_WAVE for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+        self.board = [[BOARD_WAVE for _ in range(BOARD_SIZE)]
+                      for _ in range(BOARD_SIZE)]
+        self.public_board = [[BOARD_WAVE for _ in range(BOARD_SIZE)]
+                             for _ in range(BOARD_SIZE)]
 
     # Called to place ships on the board at random locations
     def place_ships(self):
@@ -63,7 +67,7 @@ class GameBoard:
             else:
                 print(i, *self.board[i], sep=BOARD_SEP)
 
-    # Called to check if a given coordinate has already been guessed on this game board
+    # Check coordinates have already been guessed
     def check_coord_guessed(self, col: int, row: int):
         return self.public_board[row][col] in [BOARD_MISS, BOARD_HIT]
 
@@ -80,6 +84,7 @@ class GameBoard:
         self.board[row][col] = BOARD_MISS
         self.public_board[row][col] = BOARD_MISS
         return False
+
 
 def main():
     print(TITLE_STYLE + TITLE_TXT)
@@ -98,7 +103,7 @@ def main():
 
         game_loop(player_board, comp_board)
 
-        # Break from the main loop if the player doesn't wish to play another game
+        # Break from the main loop after game
         if not prompt_for_new_game():
             break
 
@@ -107,7 +112,8 @@ def main():
 
     print(Style.RESET_ALL + "\nGoodbye!")
 
-# Main execution loop of the game. Called once the game boards are setup and ready
+
+# Main execution loop of the game.
 def game_loop(player: GameBoard, comp: GameBoard):
     while True:
         # Display Player's game board and the AI's public-facing game board
@@ -116,7 +122,8 @@ def game_loop(player: GameBoard, comp: GameBoard):
 
         # Shoot at Player's desired coordinates on the AI's game board
         if comp.shoot_coordinates(*prompt_for_coordinates(comp)):
-            print(SHOT_STYLE + f"\nYour shot was a hit! {comp.name} has {comp.ships} ship(s) remaining.")
+            print(SHOT_STYLE + f"\nYour shot was a hit! {comp.name} \
+                  has {comp.ships} ship(s) remaining.")
 
             # End the game if player has destroyed the AI's last ship
             if not comp.ships:
@@ -129,15 +136,18 @@ def game_loop(player: GameBoard, comp: GameBoard):
 
         # Shoot at randomly chosen coordinates on the Player's game board
         if player.shoot_coordinates(col, row):
-            print(SHOT_STYLE +
-                  f"\n{comp.name} hit your ship at {COL_LOOKUP[col]}{row}. You have {player.ships} ship(s) remaining.")
+            print(SHOT_STYLE + f"\n{comp.name} hit your ship at \
+                  {COL_LOOKUP[col]}{row}. You have {player.ships} \
+                  ship(s) remaining.")
 
             # End the game if the AI has destroyed the Player's last ship
             if not player.ships:
                 return end_game(player, comp)
 
         else:
-            print(SHOT_STYLE + f"\n{comp.name} shot and missed at {COL_LOOKUP[col]}{row}.")
+            print(SHOT_STYLE + f"\n{comp.name} \
+                  shot and missed at {COL_LOOKUP[col]}{row}.")
+
 
 # Called once one of the player's has zero ships remaining
 def end_game(player: GameBoard, comp: GameBoard):
@@ -151,6 +161,7 @@ def end_game(player: GameBoard, comp: GameBoard):
     # Player Won
     else:
         print(PROMPT_STYLE + VICTORY_TEXT)
+
 
 # Called at the beginning of the game to check if the player needs instructions
 def prompt_for_instructions():
@@ -170,6 +181,7 @@ def prompt_for_instructions():
         else:
             print(ERROR_STYLE + ERROR_MESSAGE)
 
+
 # Called to chose random coordinates for the AI
 def chose_random_coordinates(player: GameBoard):
     x = randint(0, BOARD_SIZE - 1)
@@ -179,17 +191,19 @@ def chose_random_coordinates(player: GameBoard):
     while player.check_coord_guessed(x, y):
         x = randint(0, BOARD_SIZE - 1)
         y = randint(0, BOARD_SIZE - 1)
-    
+
     return x, y
-    
+
+
 # Called to get coordinates from the Player
 def prompt_for_coordinates(comp: GameBoard):
     while True:
         # Remove any non-word characters from the Player's input
         user_input = sub(r"\W", '', input(PROMPT_STYLE + COORDINATE_PROMPT))
-        
+
         # Ensure the given coordinates are valid
-        if len(user_input) != 2 or not user_input[0].isalpha() or not user_input[1].isnumeric():
+        if len(user_input) != 2 or not user_input[0].isalpha() \
+                or not user_input[1].isnumeric():
             print(ERROR_STYLE + COORDINATE_ERR_MSG)
             continue
 
@@ -202,15 +216,16 @@ def prompt_for_coordinates(comp: GameBoard):
         if col >= BOARD_SIZE or row >= BOARD_SIZE:
             print(ERROR_STYLE + RANGE_ERROR)
             continue
-        
+
         # Ensures the Player does not double guess a coordinate
         if comp.check_coord_guessed(col, row):
             print(ERROR_STYLE + DUPLICATE_CHOICE_ERROR)
             continue
 
         return col, row
-        
-# Called at the end of the game to check if the Player wishes to play another round
+
+
+# Called at the end if the Player plays another round
 def prompt_for_new_game():
     while True:
         user_input = input(PROMPT_STYLE + NEW_GAME_PROMPT).strip().lower()[0]
@@ -219,10 +234,10 @@ def prompt_for_new_game():
         if user_input == 'n':
             return False
         print(ERROR_STYLE + NEW_GAME_ERR_MSG)
-        
+
 
 # Number of rows and columns on the game board
-BOARD_SIZE = 8 
+BOARD_SIZE = 8
 # Number of ships to be placed on the board for each player
 NUMBER_OF_SHIPS = 4
 
@@ -231,16 +246,16 @@ BOARD_WAVE = '~'    # Coordinate containing empty space
 BOARD_SHIP = 'S'    # Coordinate containing a ship
 BOARD_MISS = 'O'    # Coordinate that has been shot at
 BOARD_HIT = 'X'     # Coordinate containing a destroyed ship
-BOARD_SEP = " | "   # Placed between columns on the board 
+BOARD_SEP = " | "   # Placed between columns on the board
 
 # Colorama styles used to display the game
-TITLE_STYLE = Fore.BLACK + Back.RED + Style.BRIGHT      # Used when displaying the Battleship title
-LOGO_STYLE = Fore.BLACK + Back.BLUE + Style.BRIGHT      # Used when displaying the Battleship ASCII art
-PROMPT_STYLE = Fore.GREEN                               # Used when prompting the user for input
-ERROR_STYLE = Fore.RED                                  # Used when displaying an error message
-MESSAGE_STYLE = Back.YELLOW + Fore.BLACK                # Used when displaying out-of-game messages
-NAMEPLATE_STYLE = Fore.CYAN + Style.BRIGHT              # Used when displaying the nameplate above the game board
-SHOT_STYLE = Fore.YELLOW                                # Used when displaying shot messages to the player
+TITLE_STYLE = Fore.BLACK + Back.RED + Style.BRIGHT
+LOGO_STYLE = Fore.BLACK + Back.BLUE + Style.BRIGHT
+PROMPT_STYLE = Fore.GREEN
+ERROR_STYLE = Fore.RED
+MESSAGE_STYLE = Back.YELLOW + Fore.BLACK
+NAMEPLATE_STYLE = Fore.CYAN + Style.BRIGHT
+SHOT_STYLE = Fore.YELLOW
 
 
 #####################################
@@ -255,14 +270,14 @@ TITLE_TXT = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"\
             "|____/_/   \_\_|   |_| |_____|_____|____/|_| |_|___|_|    \n" \
             "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  "
 
-LOGO_TEXT = "                                  # #  ( )                        \n" \
-            "                               ___#_#___|__                       \n" \
-            "                           _  |____________|  _                   \n" \
-            "                    _=====| | |            | | |==== _            \n" \
-            "              =====| |.---------------------------. | |====       \n" \
-            "<--------------------'   .  .  .  .  .  .  .  .   '--------------/\n" \
-            "  \                                                             / \n" \
-            "   \_______________________________________________WWS_________/    "
+LOGO_TEXT = "                             # #  ( )                        \n" \
+            "                          ___#_#___|__                       \n" \
+            "                      _  |____________|  _                   \n" \
+            "               _=====| | |            | | |==== _            \n" \
+            "         =====| |.---------------------------. | |====       \n" \
+            "<---------------'   .  .  .  .  .  .  .  .   '--------------/\n" \
+            "  \                                                        / \n" \
+            "   \__________________________________________WWS_________/    "
 
 VICTORY_TEXT = "__     _____ ____ _____ ___  ______   __\n" \
                "\ \   / /_ _/ ___|_   _/ _ \|  _ \ \ / /\n" \
@@ -282,23 +297,25 @@ INSTRUCTIONS_PROMPT = "\nWelcome to the game of Battleship!\n" \
 
 ERROR_MESSAGE = "\nInvalid input, please try again!"
 
-INSTRUCTIONS_TXT = "The goal of Battleship is to destroy all enemy ships before yours are destroyed.\n" \
-                   "At the start of the game, all ships will be placed randomly on the game board.\n" \
+INSTRUCTIONS_TXT = "The goal of Battleship is to destroy all enemy ships.\n" \
+                   "All ships will be placed randomly on the game board.\n" \
                    "The board consists of columns A-H and rows 0-7.\n" \
-                   "To guess the location of an enemy ship, type the coordinates for your guess into the terminal.\n" \
-                   "The first player to destroy all 4 enemy ships is the winner!"
-                   
-GAME_START_TXT = "___________________________ BEGIN GAME ___________________________"
+                   "Guess enemy ships by typing the coordinates.\n" \
+                   "Destroy all 4 enemy ships your the winner!"
+
+GAME_START_TXT = "__________ BEGIN GAME __________"
 
 NAME_PROMPT = "\nWelcome to a new game of Battleship!\n\n" \
               "Please input your name: "
 
 COORDINATE_PROMPT = "\nPlease enter the coordinates for your guess: "
 
-COORDINATE_ERR_MSG = "Invalid Input! Please enter only a letter followed by a number.\n" \
+COORDINATE_ERR_MSG = "Invalid Input!\n" \
+                     "Please enter only a letter followed by a number.\n" \
                      "Example: C4"
 
-RANGE_ERROR = "Input coordinates are out of range. Please enter only coordinates A-H and 0-7"
+RANGE_ERROR = "Input coordinates are out of range.\n" \
+              "Please enter only coordinates A-H and 0-7"
 
 DUPLICATE_CHOICE_ERROR = "You have already shot at these coordinates."
 
